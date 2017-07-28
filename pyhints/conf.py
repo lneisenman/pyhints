9893a400c6e31599e10b212776f8a896587125e3
Larry Eisenman <leisenman@wustl.edu>
2017-07-28 10:14:09 -0500

    add notebooks for combining pandas dataframes

--- pyhints/conf.py
+++ pyhints/conf.py
@@ -31,7 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax',
+extensions = ['sphinx.ext.mathjax', 'nbsphinx',
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -70,7 +70,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -168,6 +168,3 @@ texinfo_documents = [
      author, 'pyhints', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
---
+++ pyhints/pandas/pandas0.rst
@@ -0,0 +1,12 @@
+===============
+Pandas Trivia
+===============
+
+Some random Pandas stuff
+
+.. toctree::
+   :maxdepth: 2
+
+   pandas1.ipynb
+   pandas2.ipynb
+   
